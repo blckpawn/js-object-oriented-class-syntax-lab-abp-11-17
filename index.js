750462ad2fb7a601e2ab17236e3b3072dc721ad2
Brian Winter <blackpawn8990@gmail.com>
2017-11-14 01:45:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ class BoardMember{
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  
+
   }
 
     veto(){
@@ -19,6 +19,7 @@ class BoardMember{
       return "You will see great things from Scuber."
     }
     sayHi(){
-      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`
+      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
     }
 }
+
